9f7c54bd9de31722ac234f4bdc0259ab141a64f2
badatfun <j.c.delafields@gmail.com>
2018-04-12 12:50:56 -0400

    Functionalized the script. Added day of the week execution.

--- app.py
+++ app.py
@@ -1,38 +1,55 @@
 from bs4 import BeautifulSoup
 from twilio.rest import Client
 import urllib.request
+import datetime
+import sys
 import re
 import os
 
-# Soupify the page
-url = 'https://www.districtcrossfit.com/'
-page = urllib.request.urlopen(url)
-soup = BeautifulSoup(page, 'html.parser')
+def get_workout(url):
+    # Soupify the page
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page, 'html.parser')
 
-# Find the workout div and its p tags
-workout_div = soup.find('div', attrs={'class': 'summary-excerpt'})
-workout = workout_div.find_all('p')
+    # Find the workout div and its p tags
+    workout_div = soup.find('div', attrs={'class': 'summary-excerpt'})
+    workout = workout_div.find_all('p')
 
-# stringify the soup
-res = ''
-for p in workout:
-    res += str(p)
+    # stringify the soup
+    res = ''
+    for p in workout:
+        res += str(p)
 
-# sub the <br/> and <p> tags with newlines
-rm_tags = re.compile('</*(p|br)/*>')
-text = rm_tags.sub('\n', res)
+    # sub the <br/> and <p> tags with newlines
+    rm_tags = re.compile('</*(p|br)/*>')
+    text = rm_tags.sub('\n', res)
+
+    return text
+
 
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
 TWILIO_NUMBER = os.environ.get('TWILIO_NUMBER')
 MY_NUMBER = os.environ.get('MY_NUMBER')
+GYM_URL = os.environ.get('GYM_URL')
 
-client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-client.api.account.messages.create(
-    to = MY_NUMBER,
-    from_ = TWILIO_NUMBER,
-    body = text
-    )
+def text_workout():
 
-print(text)
+    text = get_workout(GYM_URL)
+
+    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+    # text the workout
+    client.api.account.messages.create(
+        to = MY_NUMBER,
+        from_ = TWILIO_NUMBER,
+        body = text
+        )
+
+weekday = datetime.datetime.today().isoweekday()
+# If its thurs or sat – exit the program, else text
+if weekday == 4 or weekday == 7:
+    sys.exit()
+else:
+    text_workout()

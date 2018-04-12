8813a7fe86189985e319b6c4ff83afa82034d1cb
badatfun <j.c.delafields@gmail.com>
2018-04-12 11:59:57 -0400

    Twilio workout text functionality added

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
+from twilio.rest import Client
 import urllib.request
 import re
+import os
 
 # Soupify the page
 url = 'https://www.districtcrossfit.com/'
@@ -19,5 +21,12 @@ for p in workout:
 # sub the <br/> and <p> tags with newlines
 rm_tags = re.compile('</*(p|br)/*>')
 text = rm_tags.sub('\n', res)
+
+account_sid = 'XXX'
+auth_token = 'XXX'
+
+client = Client(account_sid, auth_token)
+
+client.api.account.messages.create(to = '+XXX', from_ = '+XXX', body = text)
 
 print(text)
